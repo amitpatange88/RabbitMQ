9ae9b8a5d8bf8bc986d81995298b2f547cbe3743
Amit Patange <amitpatange88@gmail.com>
2018-10-25 00:12:20 +0530

    serialization and deserialization added in RabbitMQ message brokers

--- RabbitMQ/Program.cs
+++ RabbitMQ/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,14 +11,18 @@ namespace RabbitMQ
     {
         public delegate void _CallbackConsumerDel(string message);
         public static _CallbackConsumerDel _callback;
+        private static int SystemContinuosOnCount = 1;
 
         static void Main(string[] args)
         {
             Program p = new Program();
             _callback = new _CallbackConsumerDel(p.OnConsumeMessage);
-            using(RabbitMQ Rpc = new RabbitMQ())
+            SystemDetails s1 = CreateSystemObject();
+            string systemLog = SerializeJSONData(s1);
+
+            using (RabbitMQ Rpc = new RabbitMQ())
             {
-                Rpc.MessageBrokerPublish("This is test message created using RabbitMQ message broker.");
+                Rpc.MessageBrokerPublish(systemLog);
                 Rpc.MessageBrokerConsume(_callback);
             }
 
@@ -26,7 +31,72 @@ namespace RabbitMQ
 
         void OnConsumeMessage(string message)
         {
+            var details = DeserializeJSONData(message);
             Console.WriteLine(" [x] Received {0}", message);
+        }
+
+        public static SystemDetails CreateSystemObject()
+        {
+            SystemDetails s1 = new SystemDetails()
+            {
+                LogId = Guid.NewGuid(),
+                PreciseTimeStamp = DateTime.Now,
+                UTCTimeStamp = DateTime.UtcNow,
+                MachineName = System.Environment.MachineName,
+                OSVersion = System.Environment.OSVersion.ToString(),
+                Is64BitOperatingSystem = System.Environment.Is64BitOperatingSystem,
+                ProcessorCount = System.Environment.ProcessorCount,
+                OnCount = SystemContinuosOnCount,
+                ProcessesRunning = ProcessesRunningForLogs()
+            };
+
+            return s1;
+        }
+
+        /// <summary>
+        /// Serialize the JSON data. Using with Default contract resolver for non public fields.
+        /// </summary>
+        /// <param name="e"></param>
+        /// <returns></returns>
+        private static string SerializeJSONData(SystemDetails e)
+        {
+            Newtonsoft.Json.JsonSerializerSettings jss = new Newtonsoft.Json.JsonSerializerSettings();
+
+            Newtonsoft.Json.Serialization.DefaultContractResolver dcr = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            dcr.DefaultMembersSearchFlags |= System.Reflection.BindingFlags.NonPublic;
+            jss.ContractResolver = dcr;
+
+            var response = Newtonsoft.Json.JsonConvert.SerializeObject(e, jss);
+
+            return response;
+        }
+
+        private static SystemDetails DeserializeJSONData(string message)
+        {
+            Newtonsoft.Json.JsonSerializerSettings jss = new Newtonsoft.Json.JsonSerializerSettings();
+
+            Newtonsoft.Json.Serialization.DefaultContractResolver dcr = new Newtonsoft.Json.Serialization.DefaultContractResolver();
+            dcr.DefaultMembersSearchFlags |= System.Reflection.BindingFlags.NonPublic;
+            jss.ContractResolver = dcr;
+
+            var response = Newtonsoft.Json.JsonConvert.DeserializeObject<SystemDetails>(message);
+
+            return response;
+        }
+
+        private static string ProcessesRunningForLogs()
+        {
+            string processesRunning = string.Empty;
+            Process[] processes = Process.GetProcesses();
+            foreach (Process p in processes)
+            {
+                if (!String.IsNullOrEmpty(p.MainWindowTitle))
+                {
+                    processesRunning += p.MainWindowTitle + "&nbsp;";
+                }
+            }
+
+            return processesRunning;
         }
     }
 }
